699f349ee6276ef961f1476ab574f5906c3faa3c
Rihards Zamoidiks <65077992+rihitz@users.noreply.github.com>
2023-10-06 15:20:44 +0200

    Update main.py
    Added functionality of quantity takeoff

--- A2: Analysis/main.py
+++ A2: Analysis/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import ifcopenshell  
 import pandas as pd  
+import openpyxl
 
 def loadFile():
     # Specify the name of the IFC model
@@ -26,7 +27,7 @@ def loadFile():
     # Load the IFC model
     model = ifcopenshell.open(model_url)
     
-    return model
+    return model, modelname
 
 def quantityTakeOff(element):
     # Retrieve IFC elements of the specified type
@@ -102,6 +103,16 @@ def quantityTakeOff(element):
                                         if quantity.Name == 'NetVolume':
                                             element_properties['Volume, m3'] = quantity.VolumeValue
     
+                    # for definition in definitions:
+                    #     # Check if the relationship is of type 'IfcRelAssociatesMaterial'
+                    #     if definition.is_a('IfcRelAssociatesMaterial'):
+                    #         # This relationship associates materials with the element
+                    #         # Iterate through related materials
+                    #         for related_material in definition.RelatedObjects:
+                    #             if related_material.is_a('IfcMaterial'):
+                    #                 # Extract material name and add it to the list
+                    #                 element_properties['Material'] = related_material.Name
+    
                     # Append the data as a new row to the pandas DataFrame
                     df = pd.concat([df, pd.DataFrame([element_properties], columns=columns)], ignore_index=True)
                     
@@ -115,8 +126,54 @@ def quantityTakeOff(element):
 
     return df, tot_elements, floorwise_sum, total_volume, total_area, element
 
+def report(floorwise_summary, tot_quantities, modelname):
+
+    # Concatenate floorwise summary results
+    summary = pd.concat(floorwise_summary, axis=1).round(2)
+    
+    # Reset the index to apply numerical indexing and make the "Floor" index an actual column
+    summary.reset_index(level=0, inplace=True)
+    summary.index += 1  # Add 1 to the index to start indexing from 1
+
+    # Rename the columns to distinguish duplicate names
+    summary.columns = [f'{col} {i}' for i, col in enumerate(summary.columns)]
+    
+    # Convert to DataFrame
+    tot_quantities = pd.DataFrame([tot_quantities])
+    
+    # Load template and save a report file
+    wb_temp = openpyxl.load_workbook('./input/rep_template.xlsx')
+    wb_temp.save('./output/' + "report_" + modelname + '.xlsx')
+
+    report_file_path = './output/' + "report_" + modelname + '.xlsx'
+    wb_rep = openpyxl.load_workbook(report_file_path)
+
+    # Create space for reported quantities while maintaining template layout
+    for i in range(1,len(summary)): 
+        wb_rep['Sheet1'].insert_rows(6) # number indicates insertion point
+    
+    # Add index column
+    for i in range(1,len(summary)+1): 
+        wb_rep['Sheet1'].cell(row=5+i, column=2).value = i 
+
+    # Insert IFC file name
+    wb_rep['Sheet1'].cell(row=3, column=3).value = modelname + ".ifc"
+    wb_rep.save(report_file_path)
+    
+    # Create a Pandas ExcelWriter to append data
+    with pd.ExcelWriter(report_file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+
+        # Write the DataFrame to the Excel file on 'Sheet1'
+        summary.to_excel(writer, sheet_name='Sheet1', index=False, header=False, startrow=5, startcol=2)
+        tot_quantities.to_excel(writer, sheet_name='Sheet1', index=False, header=False, startrow=5+len(summary), startcol=3)
+
+    # Inform about the completion and storage location
+    print("Report has been exported to an Excel file:", report_file_path)
+    
+    return 
+
 # Load the IFC model
-model = loadFile()  
+model, modelname = loadFile()  
 
 # Output a written report within Python's console:  
 
@@ -135,20 +192,29 @@ for floor in floorNames:
 # List of structural element types to be reported
 elements = ['IfcColumn', 'IfcWall', 'IfcBeam', 'IfcSlab']
 
-# Initialize a list for a full DataFrame storage per specified element type
-element_summary = []
+# Initialize lists for result storage:
+element_summary = []        # list of DataFrames for each element
+floorwise_summary = []      # list of DataFrames for floorwise summaries
+tot_quantities = []         # total quantities by element type
 
-# Process and report each type of element
+# Process each structural element type
 for element in elements:
     element_quantities, tot_elements, floorwise_sum, total_volume, total_area , element = quantityTakeOff(element)
+    
+    # Add results to the lists
     element_summary.append(element_quantities)
+    floorwise_summary.append(floorwise_sum)
+    tot_quantities.append(total_volume)
+    tot_quantities.append(total_area)
+    
+    # Print a report in Python's console
     print('________________________________________________________________')
     print('\nOut of', tot_elements, element, 'elements', len(element_quantities), 'are load bearing (structural).')
     print('\nFloorwise summary for', element,' elements: \n')
     print(floorwise_sum, '\n')
     print('Total material quantities for', element, 'elements:')
     print('Volume: ', round(total_volume,2), 'm3')
-    print('Surface area: ', round(total_area,2), 'm2')
+    print('Surface Area: ', round(total_area,2), 'm2')
     
 # To access the full DataFrame of an element:
     # element_summary[0] - Columns
@@ -157,3 +223,22 @@ for element in elements:
     # element_summary[3] - Slabs
     
     # (to be used in further processing for price calculations)
+
+
+# Generate a spreadsheet report
+while True: 
+    prompt = input("\nDo you want to generate a spreadsheet report? (Enter 'y' for Yes, 'n' for No): ")
+    
+    if prompt == 'y':   
+        report(floorwise_summary, tot_quantities, modelname)
+        break
+    elif prompt == 'n':
+        print("Exiting without generating a report.")
+        break
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")  
+
+
+
+
+
